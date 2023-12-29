1b9be194d68657512b24ce2dc4025e2b5dc3ce3b
Parasjmail <paras.jmail1234@gmail.com>
2023-12-29 19:05:06 +0530

    This are my projects

---
+++ Ecomerce/backend/controllers/productController.js
@@ -0,0 +1,197 @@
+const Product = require("../models/prodectModel")
+const ErrorHandler = require("../utils/errorhandler")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures")
+
+//Create Product --Admin
+exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
+
+    req.body.user = req.user.id;
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    });
+});
+
+//Get All Products
+exports.getAllProducts = catchAsyncErrors(async (req,res)=>{
+
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeatures = new ApiFeatures(Product.find(),req.query)
+    .search()
+    .filter().pagination(resultPerPage);
+    
+    const products = await apiFeatures.query;
+    res.status(200).json({
+        success:true,
+        products,
+    });
+});
+
+//Get Product Details
+exports.getProductDetails = catchAsyncErrors(async(req,res,next)=>{
+   
+    const product = await Product.findById(req.params.id);
+
+
+    const productCount = await Product.countDocuments();
+
+    if(!product){
+        return next(new ErrorHandler("Product Not Found",404))
+    }
+
+    res.status(200).json({
+        success:true,
+        product,
+        productCount,
+    });
+});
+
+//Update Product
+exports.updateProduct = catchAsyncErrors(async (req,res,next)=>{
+
+    let product = Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product Not Found"
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    });
+});
+
+//Delete Product
+exports.deleteProduct = catchAsyncErrors(async(req,res,next)=>{
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product Not Found"
+        });
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success:true,
+        message:"Product Delete Successfully"
+    });
+});
+
+//Create New Review or Update the Review *
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+    
+    const { rating, comment, productId} = req.body;
+  
+    const review = {
+      user: req.user._id,
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+    };
+
+    console.log(productId,review);
+  
+    const product = await Product.findById(productId);
+
+
+  
+    const isReviewed = product.reviews.find(
+      (rev) => rev.user.toString() === req.user._id.toString()
+    );
+  
+    if (isReviewed) {
+      product.reviews.forEach((rev) => {
+        if (rev.user.toString() === req.user._id.toString())
+          (rev.rating = rating), (rev.comment = comment);
+      });
+    } else {
+      product.reviews.push(review);
+      product.numOfReviews = product.reviews.length;
+    }
+  
+    let avg = 0;
+
+    product.reviews.forEach((rev) => {
+        avg += rev.rating;
+    })
+
+    product.ratings =  avg
+    /product.reviews.length;
+  
+    await product.save({ validateBeforeSave: false });
+  
+    res.status(200).json({
+      success: true,
+    });
+  });
+
+  //Get All Reviews of a Product
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+ 
+    const product = await Product.findById(req.query.id);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found",404));
+    }
+  
+    res.status(200).json({
+        success: true,
+        reviews:product.reviews,
+      });  
+});
+
+exports.deleteReviews = catchAsyncErrors(async(req,res,next) => {
+    const product = await Product.findById(req.query.productId);
+
+    if(!product){
+        return next(new ErrorHandler("Product not found",404));
+
+    }
+
+    const reviews = product.reviews.filter(
+        rev=>rev._id.toString() !== req.query.id.toString()
+        );
+
+        let avg = 0;
+
+        reviews.forEach((rev) => {
+            avg += rev.rating;
+        })
+    
+        const ratings =  avg/reviews.length;
+
+        const numOfReviews = reviews.length;
+
+        await Product.findByIdAndUpdate(req.query.productId,{
+            reviews,
+            ratings,
+            numOfReviews,
+        },{
+            new:true,
+            runValidators:true,
+            useFindAndModify:false,
+        })
+
+    res.status(200).json({
+        success: true,
+
+      });
+})
---
+++ Ecomerce/backend/models/prodectModel.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+
+//Schema
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter product name"],
+        trim:true
+    },
+    description:{
+        type:String,
+        required:[true,"Please enter product description"]
+    },
+    price:{
+        type:Number,
+        required:[true,"Please enter product price"],
+        maxLength:[8,"Price cannot exceed 8 character"]
+    },
+    ratings:{
+        type:Number,
+        default:0
+    },
+    images:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required:[true,"Please Enter Product Category"],
+
+    },
+    stock:{
+        type:Number,
+        required:[true,"Please Enter product Stock"],
+        maxLength:[4,"Stack cannot exceed 4 characters"],
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            user:{
+                type:mongoose.Schema.ObjectId,
+                ref:"user",
+                required:true,
+            },
+            name:{
+                type:String,
+                required:true,
+            },
+            rating:{
+                type:Number,
+                required:true,
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:"user",
+        required:true,
+    },
+    crateAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+module.exports = mongoose.model("Product",productSchema);
